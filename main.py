c886f1bccea431008000358416441cdaca1170dd
Calvin Huang <calvin.peak@capslock.tw>
2022-08-30 05:46:53 +0900

    feat: add api to prepare searching source, and allow user to fuzzy matching words

--- main.py
+++ main.py
@@ -1,13 +1,105 @@
-from typing import Union
-from fastapi import FastAPI
+from curses import echo
+from operator import or_
+from os import getenv
+from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel
+from pypinyin import pinyin, lazy_pinyin, Style
+from sqlalchemy import create_engine, Column, String, Integer, or_, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+SQLALCHEMY_DATABASE_URL = getenv("SQLALCHEMY_DATABASE_URL") or "sqlite:///./sql_app.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Member(Base):
+    __tablename__ = "members"
+
+    id = Column(Integer, primary_key=True, index=True)
+    group_name = Column(String)
+    name = Column(String)
+    name_pinyin = Column(String)
+    word_count = Column(Integer)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 homeHTML = open("./index.html", "r").read()
 
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+    
+def quote(content):
+    return f'"{content}"'
+
 @app.get("/")
 def home():
     return HTMLResponse(homeHTML)
+
+class SearchReq(BaseModel):
+    text: str
+
+class SearchResp(BaseModel):
+    group_name: str
+    name: str
+    name_pinyin: str
+    score: float
+
+@app.post("/search", response_model=list[SearchResp])
+def search(input: SearchReq, db: Session = Depends(get_db)):
+    size = len(input.text)
+    pinyin_conditions = map(
+        lambda quote_str: Member.name_pinyin.like(f"%{quote_str}%"),
+        map(quote, flatten(pinyin(input.text, style=Style.NORMAL)))
+    )
+    group_count = func.count(Member.group_name)
+    result = db.query(Member, group_count).filter(or_(*list(pinyin_conditions))).group_by(Member.group_name).all()
+
+    resp = list()
+    for record in result:
+        print(record[1])
+        resp.append(
+            SearchResp(
+                group_name=record[0].group_name,
+                name=record[0].name,
+                name_pinyin=record[0].name_pinyin,
+                score=size/record[0].word_count
+            )
+        )
+    return resp
+
+class Group(BaseModel):
+    name: str
+    members: list[str]
+
+@app.post("/source")
+def source(input: list[Group], db: Session = Depends(get_db)):
+    if db.query(Member).count() > 0:
+        return JSONResponse(status_code=400, content={"messsage": "skip preparing source since the table exists"})
+
+    count = 0
+    
+    for group in input:
+        for member in group.members:
+            name_pinyin = "".join(map(quote, flatten(pinyin(member, style=Style.NORMAL))))
+            new_member = Member(group_name=group.name, name=member, name_pinyin=name_pinyin, word_count=len(member))
+            db.add(new_member)
+            db.commit()
+            db.refresh(new_member)
+            count += 1
+
+    return JSONResponse(content={"created": count})
